167f059a52952ec284652fab412961ca99325739
Rishabh Singh <rishabh.singh@dunzo.in>
2022-08-22 15:58:48 +0530

    Welcome message

--- main.go
+++ main.go
@@ -2,10 +2,14 @@
 
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	a := App{}
+	fmt.Println("Project begin")
 	a.Initialize(
 		os.Getenv("APP_DB_USERNAME"),
 		os.Getenv("APP_DB_PASSWORD"),
